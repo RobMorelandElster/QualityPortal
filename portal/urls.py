e35b0a5088c668824a3196d993781098bb931b19
Richard Carpenter <richard.c.carpenter@gmail.com>
2014-08-08 10:34:45 -0400

    find static files

--- portal/urls.py
+++ portal/urls.py
@@ -19,3 +19,7 @@ urlpatterns = patterns('',
     
     url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
+
+urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
