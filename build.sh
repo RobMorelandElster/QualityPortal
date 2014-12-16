edf5535d7062fd3c8edd83089528733a7c16def7
Mark Heinze <mheinze@rad-build.ral.elsterus.com>
2014-12-16 13:10:45 -0500

    Begin work on a build tool. This is a script for Jenkins.

---
+++ build.sh
@@ -0,0 +1,16 @@
+export DJANGO_DEBUG=1
+export DATABASE_URL="postgres://elsterdev:elsterdev123@localhost/elster_qp"
+export SENDGRID_PASSWORD="ignore"
+export SENDGRID_USERNAME="elster"
+export AWS_S3_ACCESS_KEY_ID="blah_blah"
+export AWS_S3_SECRET_ACCESS_KEY=" blah_blah "
+export AWS_STORAGE_BUCKET_NAME="elster-qp"
+export DJANGO_SITE_ID=1
+
+virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
+python manage.py validate
+python manage.py test
+deactivate
