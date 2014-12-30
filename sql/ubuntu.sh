f8d23ff420464d76c2e21e4d83de0953fc72fc8b
Richard Carpenter <richard.c.carpenter@gmail.com>
2014-12-30 11:52:14 -0500

    remove sql folder with passwd stuff

--- sql/ubuntu.sh
+++
@@ -1,7 +0,0 @@
-sudo apt-get install postgresql postgresql-contrib
-echo
-sudo echo "Enter the root password for postgres"
-echo
-echo "\password postgres" | sudo -u postgres psql postgres
-sudo -u postgres createuser --superuser elsterdev
-sudo -u postgres psql -c "alter user elsterdev password 'elsterdev123'" 
